61a6920a013042589a0e5747911f99ef7f49abc8
Igor Kobelev <kia1809@yandex.ru>
2024-11-10 23:26:32 +0300

    mobile version is almost complete

--- js/feedback.js
+++ js/feedback.js
@@ -1,16 +1,11 @@
 'use strict'
 
-const onFeedbackForm = document.getElementById('button-mail');
-const offFeedbackForm = document.querySelector(
-    '.forms_feedback__button-close'
+document.getElementById('button-mail').addEventListener(
+    'click',
+    () => document.querySelector('.forms').classList.remove('remove-element')
 );
 
-onFeedbackForm.addEventListener('click', () => {
-    console.log('открыли');
-    document.querySelector('.forms').classList.remove('remove-element');
-});
-
-offFeedbackForm.addEventListener('click', () => {
-    console.log('скрыли');
-    document.querySelector('.forms').classList.add('remove-element');
-});
+document.querySelector('.forms_feedback__button-close').addEventListener(
+    'click',
+     () => document.querySelector('.forms').classList.add('remove-element')
+);
